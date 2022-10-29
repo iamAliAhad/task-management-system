955d9619af5bcdea84f97b889ab2706233b1982b
Ali <90032081+iamaliahad@users.noreply.github.com>
2022-10-29 12:43:52 +0800

    vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: true,
     proxy: {
       '/api': {
         target:'https://task-management-system-production.up.railway.app',
