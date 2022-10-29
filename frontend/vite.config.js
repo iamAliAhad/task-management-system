289a42376e9309952041847840e50336727c46d0
Ali <90032081+iamaliahad@users.noreply.github.com>
2022-10-29 13:11:35 +0800

    vite up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: true,
     proxy: {
       '/api': {
         target:'https://task-management-system-production.up.railway.app',
